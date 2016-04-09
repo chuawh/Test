5970651e6f02f0080b1e64631718eb9faf540d0b
chuawh <chuawh.74@gmail.com>
2016-04-09 17:27:54 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 //Tropo demo script
 
 //Incoming Call
-say("https://www.tropo.com/docs/troporocks.mp3");
+/*say("https://www.tropo.com/docs/troporocks.mp3");
 say("Thanks for calling Tropo.",{voice: "veronica"});
+*/
+
+//Asking Questions
+
+var result = ask("What's your favorite color?  Choose from red, blue or green.", {
+   choices:"red, blue, green”
+});
+say("You said " + result.value);
