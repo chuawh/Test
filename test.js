da022790403fd572249b4140126bc40f4d14ad12
chuawh <chuawh.74@gmail.com>
2016-04-09 17:37:45 +0800

    Update test.js

--- test.js
+++ test.js
@@ -6,8 +6,13 @@ say("Thanks for calling Tropo.",{voice: "veronica"});
 */
 
 //Asking Questions
-
+/*
 var result = ask("What's your favorite color?  Choose from red, blue or green.", {
    choices:"red, blue, green"
 });
 say("You said" + result.value);
+*/
+
+/Answering Incoming SMS
+say("We have received your SMS" +'\n');
+say("You asked about " + currentCall.InitialText)
