ed8eb3a963eb9c63ade7bf55560a19fdcf2c483f
chuawh <chuawh.74@gmail.com>
2016-04-15 13:10:35 +0800

    Update test.js

--- test.js
+++ test.js
@@ -43,7 +43,7 @@ message(textMsg, {
 */
 
 say("One potato, two potato, three potato, four potato.");
-say("<speak><prosody rate='slow'>One potato, two potato, three potato, four potato.</prosody></speak>");
+say("<speak><prosody rate='x-slow'>One potato, two potato, three potato, four potato.</prosody></speak>");
 //say("<speak><voice gender="male">Mary had a little lamb,</voice></speak>");
 //say("<speak><voice gender="female">Mary had a little lamb,</voice></speak>");
-say("<speak><prosody rate='x-fast'>One potato, two potato, three potato, four potato.</prosody></speak>");
+say("<speak><prosody rate='fast'>One potato, two potato, three potato, four potato.</prosody></speak>");
