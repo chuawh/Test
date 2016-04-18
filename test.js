165b9ba7647c6d113118bb1b52db5ff95a5cdcbf
chuawh <chuawh.74@gmail.com>
2016-04-18 13:19:14 +0800

    Update test.js

--- test.js
+++ test.js
@@ -116,8 +116,9 @@ var result=ask("你最喜欢什么颜色,红色，蓝色，绿色", {
 say(result.value,{voice:"Sin-ji"});
 */
 
-var result = ask("Pick a number from 0 to 9", {voice:"veronica"}, {
-   choices:"0,1,2,3,4,5,6,7,8,9"
+var result = ask("Pick a number from 0 to 9", {
+   choices:"0,1,2,3,4,5,6,7,8,9",
+   voice:"veronica"
 });
 say("You said" + result.value, {voice:"veronica"});
 
