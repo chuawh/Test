fa26ad02088cf4a4f047452accd6deaf86f3a415
chuawh <chuawh.74@gmail.com>
2016-04-26 22:24:07 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 //Tropo demo script
 
 //Incoming Call
-say("Tropo is easy https://www.tropo.com/static/audio/tropo-rocks.mp3");
+say("Tropo is so easy https://www.tropo.com/static/audio/tropo-rocks.mp3");
 
-message("We have received your SMS.", {
+/*message("We have received your SMS.", {
     to:"+6597809414",
     network:"SMS"
 });
-    
+*/    
 //say("Thanks for calling Tropo.",{voice: "veronica"});
 
 
